7640d206b86fecb39cfe3e3bc599ab6bad0f50d5
Yein <dpdls0603@naver.com>
2020-04-07 14:01:40 +0900

    4/7

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,22 @@
 package webserver;
 
+import java.io.BufferedReader;
+import java.io.DataOutput;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.Flushable;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,28 +29,45 @@ public class RequestHandler extends Thread {
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
-
+////코드 수정
+   
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "안녕하세요 송예인 입니다".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
+        	
+            try(InputStream in = connection.getInputStream(); OutputStream out= connection.getOutputStream()){
+            	BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+            	
+            	String firstLine      = br.readLine();
+                String arrFirstLine[] = firstLine.split(" ");
+                if(arrFirstLine.length==3){
+                String httpMethod     = arrFirstLine[0];
+                String httpUrl        = arrFirstLine[1].equals("/")?"/index.html":arrFirstLine[1];
+                String httpProtocol   = arrFirstLine[2];
+                
+            
+            	System.out.println("arrFistLine[0]: "+httpMethod);
+            	System.out.println("arrFistLine[1]: "+httpUrl);
+            	System.out.println("arrFistLine[2]: "+httpProtocol);
+                
+            	DataOutputStream dos = new DataOutputStream(out);
+            	byte[] body = Files.readAllBytes(new File("./webapp"+httpUrl).toPath());
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+                
+                }
+            }catch(IOException e) {
+            	e.printStackTrace();
+            }
+            
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
+             dos.writeBytes("HTTP/1.1 200 OK \r\n");
+             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -46,8 +75,9 @@ public class RequestHandler extends Thread {
 
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
-            dos.write(body, 0, body.length);
-            dos.flush();
+             dos.write(body, 0, body.length);
+             dos.writeBytes("\r\n");
+             dos.flush();
         } catch (IOException e) {
             log.error(e.getMessage());
         }
