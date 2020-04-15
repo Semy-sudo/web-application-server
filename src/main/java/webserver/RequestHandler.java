d6c453dd84caca3d732b0e831266ff9ad144c37c
Yein <dpdls0603@naver.com>
2020-04-15 18:11:50 +0900

    요구사항3번

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,25 +1,29 @@
 package webserver;
-
 import java.io.BufferedReader;
 import java.io.DataOutput;
-import java.io.DataOutputStream;
+import java.io.DataOutputStream;	
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.Flushable;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.IOException;	
+import java.io.InputStream;	
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.Socket;
+import java.io.OutputStream;	
+import java.net.Socket;	
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
-import org.slf4j.Logger;
+import org.slf4j.Logger;	
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;	
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -50,6 +54,37 @@ public class RequestHandler extends Thread {
             	System.out.println("arrFistLine[1]: "+httpUrl);
             	System.out.println("arrFistLine[2]: "+httpProtocol);
                 
+            	//요구사항2번
+            	HttpRequestUtils hr = new HttpRequestUtils();
+            	Map<String, String> r = hr.parseQueryString(httpUrl);//{ 정보들...   }
+            	r.forEach((k,v) -> System.out.println("key : " + k + ", value : " + v));
+            	//key 로 value찾기
+            	System.out.println(getKey(r,"syi9595"));// /user/create?userId
+            	//null
+            	//String url = getKey(r,"syi9595");
+            	//System.out.println(url);f//null
+            	
+            	//String[] s = url.split("?");
+           
+            	/*
+            	System.out.println(s[0]);
+             	System.out.println(s[1]);
+              */
+            	//User클래스에 저장
+            	String getName = (String)r.get("name");
+            	String getID = (String)r.get("/user/create?userId");
+            	String getPassword = (String)r.get("password");
+            	String getEmail = (String)r.get("email");
+            	
+            	User user = new User(getID,getPassword,getName,getEmail);
+            	System.out.println(user.toString());
+            	//요구사항3번
+            	IOUtils io = new IOUtils();
+            	String Line      = br.readLine();
+            	System.out.println(Line);
+            	io.readData(br, 59);
+            	
+            	
             	DataOutputStream dos = new DataOutputStream(out);
             	byte[] body = Files.readAllBytes(new File("./webapp"+httpUrl).toPath());
                 response200Header(dos, body.length);
@@ -62,7 +97,21 @@ public class RequestHandler extends Thread {
             
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+   
+
+
+	private static <K,V> K getKey(Map<K, V> r, V string) {
+	
+		 for (K key : r.keySet()) {
+	            if (string.equals(r.get(key))) {
+	                return key;
+	            }
+	        }
+
+		
+	return null;
+}
+	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
              dos.writeBytes("HTTP/1.1 200 OK \r\n");
              dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
